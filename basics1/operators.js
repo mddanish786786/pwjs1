07d24ea890dc5f47ae003e3f561aaf14a5887e15
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2023-12-03 10:16:22 +0000

    discuss on operators

---
+++ basics1/operators.js
@@ -0,0 +1,21 @@
+//arthematic operatos
+
+// 5+5
+// 4-3
+// 8/4
+// 4*2
+//powers  2**3 =>2*2*2
+//Reaminder 10%3=>1
+
+const numberOne = 4
+const numberTwo = 6
+
+const numberThree = numberOne+numberTwo
+
+console.log(numberThree)
+
+
+//Assignment Operators
+
+let valueTwo = 3
+valueTwo +=10
