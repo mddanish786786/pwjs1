037709e7dbebbb32664d38e4f6a8d18dc4a143a7
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2023-12-06 10:44:37 +0000

    discussion on else-if statement

---
+++ control2/else-if.js
@@ -0,0 +1,31 @@
+//practice else-if
+
+//build a rating system for pizzahut
+//1 star - poor 
+//2 star - emm, ok
+//3 star - average
+//4 star - cool 
+//5 star - great
+//6 star - awesome
+//7 star - nailed it !!!
+
+let getStarFromUser = "3"
+let starInNumber = parseInt(getStarFromUser)
+
+if (starInNumber==1){
+    console.log("poor");
+} else if(starInNumber==2){
+    console.log("emm, ok");
+}else if(starInNumber==3){
+    console.log("average");
+}else if(starInNumber==4){
+    console.log("cool");
+}else if(starInNumber==5){
+    console.log("great");
+}else if(starInNumber==6){
+    console.log("awesome");
+}else if(starInNumber==7){
+    console.log("you nailed it !!!");
+} else {
+    console.log("i dont know that rating")
+}
