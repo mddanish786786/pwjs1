600fc008b746b276c54cd05ea46ea87c31a399e3
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2023-12-08 19:16:43 +0000

    practice on function

---
+++ functions4/practiceOnFunction.js
@@ -0,0 +1,17 @@
+// Q:- write a function to calculate sum of three numbers passed as an argument return the sum as output
+
+function addNumbers(a, b, c){
+    const sumValue = a+b+c;
+    return sumValue;
+}
+
+console.log(addNumbers(10, 20, 30));
+
+//it can be written as 
+
+function addNumbers(a, b, c){
+//without storing the value
+    return sumValue;
+}
+
+console.log(addNumbers(10, 20, 30));
