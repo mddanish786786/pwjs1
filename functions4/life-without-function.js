043251df9e8905e54cbc3619ad6f3fe04b92b547
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2023-12-08 14:32:39 +0000

    discussion on life without function in Js

---
+++ functions4/life-without-function.js
@@ -0,0 +1,25 @@
+// you wrote an algorithm to render a movie screen
+//nepolean
+
+
+// you wrote an algorithm to render a movie screen
+//tiger3
+
+
+// you wrote an algorithm to render a movie screen
+//animal
+
+
+// you wrote an algorithm to render a movie screen
+//sam bahadur
+
+
+/* if a bunch of code will be copied paste with the same logic and 
+without function then problem will be occured when we will required to 
+change something in the code. this will let you to change all of the copied 
+codes will take lots of time and programing ambiguity. so the the life without function 
+will become so hectic and inappropriate production life cycle.
+
+Hence function is most important part of a programing environment 
+to reusiblity of the code.
+*/
