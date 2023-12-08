f6e3191bdfd406414958cc38a6c8a850b41de31f
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2023-12-08 18:54:25 +0000

    discussion on consolelog inside the function

---
+++ functions4/log-inside-the-function.js
@@ -0,0 +1,9 @@
+function calculateSquare(num){
+    //logic to calculate square of a number
+
+    let squarevalue = num * num;
+
+    console.log(squarevalue); // instead of return we did a log
+}
+calculateSquare(9);
+calculateSquare(5);
