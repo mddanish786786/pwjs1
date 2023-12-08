d6189ff5264144584f661103ccef7fe943bdbdff
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2023-12-08 18:20:21 +0000

    discussion on life with function

---
+++ functions4/life-with-function.js
@@ -0,0 +1,15 @@
+function calculateSquare(num){
+    //logic to calculate square of a number
+
+    let squarevalue = num * num;
+
+    //return the output of the square value
+
+    return squarevalue;
+}
+let result1 = calculateSquare(9);
+console.log(result1);
+
+//or
+
+console.log(calculateSquare(5));
