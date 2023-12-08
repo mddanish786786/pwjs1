0b6f6cf16ab5a3a3b0bad0098bb01eb668bc0603
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2023-12-08 18:58:17 +0000

    discussion on parameters and arguments

---
+++ functions4/parametersAndarguments.js
@@ -0,0 +1,10 @@
+function calculateSquare(num){
+    //here num is a parameter 
+    //logic to calculate square of a number
+
+    let squarevalue = num * num;
+
+    console.log(squarevalue); // instead of return we did a log
+}
+calculateSquare(9); // here nine is an argument
+calculateSquare(5);// here five is an argument
