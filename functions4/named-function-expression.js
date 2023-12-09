0d01e9f7da58837fca58be298d000541f8f0e3e2
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2023-12-09 13:07:21 +0000

    discussion on named function expression

---
+++ functions4/named-function-expression.js
@@ -0,0 +1,12 @@
+const isEvenOrOdd = function process(num){
+    return num%2 ==0;
+}
+
+// is this a named function expression?
+//Yes
+
+//how to call it?
+//you can use the name of the variable inside which you have stored the function to call it.
+
+const result = isEvenOrOdd(50);
+console.log(result);
