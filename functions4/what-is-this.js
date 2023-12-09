db5c62023b23e569a6ec677103cdeaa153c6e07f
MD Danish <116485272+mddanish786786@users.noreply.github.com>
2023-12-09 12:57:53 +0000

    disscussion on type of function expression

---
+++ functions4/what-is-this.js
@@ -0,0 +1,23 @@
+function fun(){   // function declaration
+
+}
+
+const f = function process(){  // named function expression
+
+}
+
+const g = function (){  // anonymous function expression
+
+}
+
+const h = () => {  // function expression
+
+}
+
+(function (){  // iife function expression
+
+})()
+
+(function exec(){  // iife function expression
+
+})()
